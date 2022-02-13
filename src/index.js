4cbd32cd30adce52291eaf328b070f339207c39f
peakhold <99507613+peakhold@users.noreply.github.com>
2022-02-13 16:00:51 +0900

    スプレッドなど

--- src/index.js
+++ src/index.js
@@ -59,10 +59,10 @@ import "./styles.css";
  * アロー関数
  */
 // 従来の関数
-const func1 = function func1(str) {
-  return str;
-};
-console.log(func1("func1です"));
+// const func1 = function func1(str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
 
 // アロー関数
 // const func2 = (str) => {
@@ -77,10 +77,67 @@ console.log(func1("func1です"));
 /**
  * 分割代入
  */
-const myProfile = {
-  name: "じゃけぇ",
-  age: 28
-};
+// const myProfile = {
+//   name: "じゃけぇ",
+//   age: 28,
+// };
 
-const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
-console.log(message1);
+// const message1 = `名前は${myProfile.name}ですyo。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// const myProile = ["じゃけぇ", 28];
+
+// const message3 = `名前は${myProile[0]}です。年齢は${myProile[1]}歳です。`;
+// console.log(message3);
+
+// const [name, age] = myProile;
+// const message4 = `名前は${name}ですyo。年齢は${age}歳です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+// const sayHello = (name ="ゲスト") => console.log(`こんにちわ！${name}さん`);
+// sayHello();
+
+/**
+ * スプレッド構文 ...
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// //sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピーや結合
+//const arr4 = [10, 20];
+//const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+////const arr8 = arr4;
+//console.log(arr8);
+//arr4[0] = 100;
+//console.log(arr8);
+//console.log(arr4);
+
+let atai = [10, 20];
+console.log(atai);
+atai[0] = 100;
